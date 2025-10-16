c860bc51cc7c90831f338079859f8d8129e4b122
Alessandro Baccelli <alessandro.baccelli17@gmail.com>
2025-10-16 12:43:05 +0200

    var binding

--- corso-7-ore/main.py
+++ corso-7-ore/main.py
@@ -2,9 +2,41 @@
 
 # Metodo per mandare a schermo un messaggio
 
-messaggio = input("Inserisci un messaggio: ")
-print(messaggio)
+# messaggio = input("Inserisci un messaggio: ")
+# print(messaggio)
 
 if 1 < 5:
     print("fatto if")
     print("sono una seconda indentazione")
+
+x = 6
+y = 5
+
+# Non posso dichiarare a priopri una var senza assegnarle contenuto
+# z
+
+# Non posso scrivere formati di variabile così
+# 2pesopersona
+# peso-persona
+# peso persona
+
+# Standard dichiarazione var:
+peso_persona_amichevole = 45
+# Si usa l'underscore piuttosto che il camelCase, ma nulla lo vieta a prescindere
+
+#Multidichiarazione
+x, y, z = 32, 45, 100
+
+print(x)
+print(y)
+
+# Qua hanno tutte lo stesso valore, si assegna così
+a = b = c = 99
+print(a, b, c)
+
+# Array
+città = ["Roma", "Genova", "Tirana"]
+# A ogni var assegna un valore dell'array
+x, y, z = città
+# Notare che x, y, z cambiano di tipo da int a stringhe
+print(x, y, z)
