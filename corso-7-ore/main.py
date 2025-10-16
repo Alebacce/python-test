a19eadb8ca489ff3f6c83835d3bf0e86a0abb7c8
Alessandro Baccelli <alessandro.baccelli17@gmail.com>
2025-10-16 12:25:47 +0200

    Main.py + refactore gitIgnore

---
+++ corso-7-ore/main.py
@@ -0,0 +1,10 @@
+# Sono un commento
+
+# Metodo per mandare a schermo un messaggio
+
+messaggio = input("Inserisci un messaggio: ")
+print(messaggio)
+
+if 1 < 5:
+    print("fatto if")
+    print("sono una seconda indentazione")
